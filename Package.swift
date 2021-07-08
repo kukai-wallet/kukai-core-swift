30ed1c079a6ff559d08d9c21b09b91a8f0dad278
Simon McLoughlin <simon.d.mcl@gmail.com>
2021-07-08 12:58:30 +0100

    - get Torus library into package and compiling - can't remove 2 warnings, opened another github issue for Torus team

--- Package.swift
+++ Package.swift
@@ -12,11 +12,11 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/attaswift/BigInt.git", from: "5.2.1"),
 		.package(name: "Sodium", url: "https://github.com/jedisct1/swift-sodium.git", from: "0.9.1"),
-		.package(name: "secp256k1", url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.4"),
+		//.package(name: "secp256k1", url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.4"),
 		.package(url: "https://github.com/onevcat/Kingfisher.git", from: "6.3.0"),
 		.package(name: "WalletCore", url: "https://github.com/hewigovens/wallet-core-spm", .branch("master")),
-		//.package(url: "https://github.com/rathishubham7/web3swift", .branch("develop")),
-		//.package(url: "https://github.com/torusresearch/torus-direct-swift-sdk", from: "0.3.1"),
+		//.package(name: "secp256k1", url: "https://github.com/rathishubham7/web3swift", from:"2.0.0"),
+		.package(name: "TorusSwiftDirectSDK", url: "https://github.com/torusresearch/torus-direct-swift-sdk", from: "0.3.1"),
     ],
     targets: [
         .target(
@@ -25,9 +25,10 @@ let package = Package(
 				.product(name: "Clibsodium", package: "Sodium"),
 				"Sodium",
 				"BigInt",
-				"secp256k1",
+				//"secp256k1",
 				"Kingfisher",
 				"WalletCore",
+				"TorusSwiftDirectSDK"
 			],
 			resources: [
 				.copy("Services/External")
