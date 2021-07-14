bbb97a758adbd33ce91aae22b1f8fa5470234159
Simon McLoughlin <simon.d.mcl@gmail.com>
2021-07-14 14:04:06 +0100

    temporary workaround to swift sodium issues

--- Package.swift
+++ Package.swift
@@ -11,7 +11,9 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/attaswift/BigInt.git", from: "5.2.1"),
-		.package(name: "Sodium", url: "https://github.com/jedisct1/swift-sodium.git", from: "0.9.1"),
+		
+		// TODO: problem with the offical swift-sodium (https://github.com/jedisct1/swift-sodium), using someone elses fork with a bug fix until merged into main repo or SPM bug fixed
+		.package(name: "Sodium", url: "https://github.com/junelife/swift-sodium.git", .branch("master")),
 		.package(url: "https://github.com/onevcat/Kingfisher.git", from: "6.3.0"),
 		.package(name: "WalletCore", url: "https://github.com/hewigovens/wallet-core-spm", .branch("master")),
 		.package(name: "TorusSwiftDirectSDK", url: "https://github.com/simonmcl/torus-direct-swift-sdk", .branch("master")),
@@ -20,7 +22,7 @@ let package = Package(
         .target(
 			name: "KukaiCoreSwift",
 			dependencies: [
-				.product(name: "Clibsodium", package: "Sodium"),
+				//.product(name: "Clibsodium", package: "Sodium"),
 				"Sodium",
 				"BigInt",
 				"Kingfisher",
