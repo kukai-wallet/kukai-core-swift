0fee4b62c7ec9abce890da9f51664db3744e2cdd
Simon McLoughlin <simon.d.mcl@gmail.com>
2021-07-23 16:33:10 +0100

    package file clean up

--- Package.swift
+++ Package.swift
@@ -4,34 +4,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "KukaiCoreSwift",
+	name: "KukaiCoreSwift",
 	platforms: [.iOS(.v14)],
-    products: [
+	products: [
 		.library(name: "KukaiCoreSwift", targets: ["KukaiCoreSwift"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/attaswift/BigInt.git", from: "5.2.1"),
+	],
+	dependencies: [
+		.package(url: "https://github.com/attaswift/BigInt.git", from: "5.2.1"),
 		
 		// TODO: problem with the offical swift-sodium (https://github.com/jedisct1/swift-sodium), using someone elses fork with a bug fix until merged into main repo or SPM bug fixed
 		.package(name: "Sodium", url: "https://github.com/junelife/swift-sodium.git", .branch("spm")),
 		.package(url: "https://github.com/onevcat/Kingfisher.git", from: "6.3.0"),
 		.package(name: "WalletCore", url: "https://github.com/hewigovens/wallet-core-spm", .branch("master")),
 		.package(name: "TorusSwiftDirectSDK", url: "https://github.com/simonmcl/torus-direct-swift-sdk", .branch("master")),
-    ],
-    targets: [
-		/*.binaryTarget(
-			name: "Clibsodium",
-			path: "Clibsodium.xcframework"
-		),
-		.binaryTarget(
-			name: "Sodium",
-			path: "Sodium.xcframework"
-		),*/
-        .target(
+	],
+	targets: [
+		.target(
 			name: "KukaiCoreSwift",
 			dependencies: [
 				//.product(name: "Clibsodium", package: "Sodium"),
-				//"Clibsodium",
 				"Sodium",
 				"BigInt",
 				"Kingfisher",
@@ -43,12 +34,12 @@ let package = Package(
 			]
 		),
 		
-        .testTarget(
+		.testTarget(
 			name: "KukaiCoreSwiftTests",
 			dependencies: ["KukaiCoreSwift"],
 			resources: [
 				.copy("Stubs")
 			]
 		),
-    ]
+	]
 )
