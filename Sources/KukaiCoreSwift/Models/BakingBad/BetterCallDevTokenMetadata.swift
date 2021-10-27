d752941605f5fa4baef2d8852d1e1c16ebc30b95
Simon McLoughlin <simon.d.mcl@gmail.com>
2021-10-27 14:06:35 +0100

    BCD metadata change

--- Sources/KukaiCoreSwift/Models/BakingBad/BetterCallDevTokenMetadata.swift
+++ Sources/KukaiCoreSwift/Models/BakingBad/BetterCallDevTokenMetadata.swift
@@ -14,8 +14,8 @@ public class BetterCallDevTokenMetadata: Codable {
 	public let contract: String
 	public let network: String
 	public let token_id: Int
-	public let symbol: String
-	public let name: String
+	public let symbol: String?
+	public let name: String?
 	public let decimals: Int
 	
 	public var faVersion: FaVersion?
