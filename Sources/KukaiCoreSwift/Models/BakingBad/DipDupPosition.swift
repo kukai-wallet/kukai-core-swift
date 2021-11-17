49a6e2aa68f2cd95f1def076450a2c9b363a9313
Simon McLoughlin <simon.d.mcl@gmail.com>
2021-11-17 12:18:01 +0000

    add hashable and equatable to dip dup position

--- Sources/KukaiCoreSwift/Models/BakingBad/DipDupPosition.swift
+++ Sources/KukaiCoreSwift/Models/BakingBad/DipDupPosition.swift
@@ -11,13 +11,24 @@ public struct DipDupPosition: Codable {
 	public let position: [DipDupPositionData]
 }
 
-public struct DipDupPositionData: Codable {
+public struct DipDupPositionData: Codable, Hashable, Equatable {
 	public let sharesQty: String
 	public let token: DipDupToken
 	public let exchange: DipDupExchange
 	
 	public func tokenAmount() -> TokenAmount {
 		return TokenAmount(fromRpcAmount: sharesQty, decimalPlaces: token.decimals) ?? TokenAmount.zero()
+	}
+	
+	/// Conforming to `Hashable` to enable working with UITableViewDiffableDataSource
+	public func hash(into hasher: inout Hasher) {
+		hasher.combine(exchange.name)
+		hasher.combine(token.address)
+	}
+	
+	/// Conforming to `Equatable` to enable working with UITableViewDiffableDataSource
+	public static func == (lhs: DipDupPositionData, rhs: DipDupPositionData) -> Bool {
+		return lhs.exchange.name == rhs.exchange.name && lhs.token.address == rhs.token.address
 	}
 }
 
