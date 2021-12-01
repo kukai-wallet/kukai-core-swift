48e84aa185008eb4cc3b035d93cd40657dff34dc
Simon McLoughlin <simon.d.mcl@gmail.com>
2021-12-01 14:38:56 +0000

    move to official SPM version of walletCore

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
 		.package(url: "https://github.com/attaswift/BigInt.git", from: "5.2.1"),
 		.package(name: "Sodium", url: "https://github.com/jedisct1/swift-sodium", from: "0.9.1"),
 		.package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.0.0"),
-		.package(name: "WalletCore", url: "https://github.com/hewigovens/wallet-core-spm", .branch("master")),
+		.package(name: "WalletCore", url: "https://github.com/trustwallet/wallet-core", from: "2.6.34"),
 		.package(name: "TorusSwiftDirectSDK", url: "https://github.com/torusresearch/torus-direct-swift-sdk", from: "1.1.1"),
 		.package(url: "https://github.com/simonmcl/SVGKit", from: "3.0.1"),
 		.package(name: "SignalRClient", url: "https://github.com/moozzyk/SignalR-Client-Swift", from: "0.8.0")
