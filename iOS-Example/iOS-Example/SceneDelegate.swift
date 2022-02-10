e63f414c8198d2cdb65430f2924310bb2625d1d2
Simon McLoughlin <simon.d.mcl@gmail.com>
2022-02-10 14:35:40 +0000

    remove reference to deleted class

--- iOS-Example/iOS-Example/SceneDelegate.swift
+++ iOS-Example/iOS-Example/SceneDelegate.swift
@@ -15,7 +15,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 	var window: UIWindow?
 	var bag = Set<AnyCancellable>()
-	var tools: TezToolsClient? = nil
 
 	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 		// Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
