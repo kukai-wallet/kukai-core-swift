3e53a2e6ca542778ba78506aba8c64832ca20f93
Simon McLoughlin <simon.d.mcl@gmail.com>
2023-07-14 10:46:05 +0100

    second experiment

--- iOS-Example/Package.swift
+++ iOS-Example/Package.swift
@@ -2,5 +2,3 @@
 // This is a dummy/empty Package declartion to trigger an undocumented Xcode "feature" of hiding a directory from the navigator when imported, so various assets aren't pickuped up and made available when they shouldn't be
 
 import PackageDescription
-
-let package = Package()
