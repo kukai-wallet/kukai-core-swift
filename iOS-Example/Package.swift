08597ee4fbd8bbf52945ce730a89ef8066b319d0
Simon McLoughlin <simon.d.mcl@gmail.com>
2023-07-14 10:37:29 +0100

    experiment to see can we filter out unwanted resources from SPM when imported via xcode

---
+++ iOS-Example/Package.swift
@@ -0,0 +1,6 @@
+// swift-tools-version:5.5
+// This is a dummy/empty Package declartion to trigger an undocumented Xcode "feature" of hiding a directory from the navigator when imported, so various assets aren't pickuped up and made available when they shouldn't be
+
+import PackageDescription
+
+let package = Package()
