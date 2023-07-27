3072010d876eee8e7b83294160ef875d454d4fc3
Simon McLoughlin <simon.d.mcl@gmail.com>
2023-07-27 07:51:43 +0100

    bump torus sdk to see if it resolves issues

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
 		.package(name: "KukaiCryptoSwift", url: "https://github.com/kukai-wallet/kukai-crypto-swift", from: "1.0.7"),
 		//.package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.6.2"),
 		.package(url: "https://github.com/simonmcl/Kingfisher.git", from: "1.0.0"),
-		.package(name: "CustomAuth", url: "https://github.com/torusresearch/customauth-swift-sdk", from: "5.0.0"),
+		.package(name: "CustomAuth", url: "https://github.com/torusresearch/customauth-swift-sdk", from: "6.0.0"),
 		.package(url: "https://github.com/simonmcl/SVGKit", from: "3.0.3"),
 		.package(name: "SignalRClient", url: "https://github.com/moozzyk/SignalR-Client-Swift", from: "0.8.0"),
 	],
