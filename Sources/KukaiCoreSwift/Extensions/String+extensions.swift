51eff6615cf764efbc607cfcbbb583789680ad14
Simon McLoughlin <simon.d.mcl@gmail.com>
2023-11-08 16:24:08 +0000

    remove unnecessary md5 function

--- Sources/KukaiCoreSwift/Extensions/String+extensions.swift
+++ Sources/KukaiCoreSwift/Extensions/String+extensions.swift
@@ -10,15 +10,6 @@ import CryptoKit
 
 public extension String {
 	
-	/// Generate an MD5 hash from the string
-	func md5() -> String {
-		let digest = Insecure.MD5.hash(data: data(using: .utf8) ?? Data())
-
-		return digest.map {
-			String(format: "%02hhx", $0)
-		}.joined()
-	}
-	
 	/// Return the starting indexes of each occurnace of the supplied string
 	func indexesOf(string: String) -> [String.Index] {
 		var searchRange = self.startIndex..<self.endIndex
