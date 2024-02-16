94c5085bf53877044bb568a97e4a895c5d4df4e0
Simon McLoughlin <simon.d.mcl@gmail.com>
2024-02-16 10:40:04 +0000

    add serviceType to baking-bad baker data

--- Sources/KukaiCoreSwift/Models/BakingBad/TzKTBaker.swift
+++ Sources/KukaiCoreSwift/Models/BakingBad/TzKTBaker.swift
@@ -30,6 +30,14 @@ public enum TzKTBakerTiming: String, Codable {
 	case no_data
 }
 
+/// The reliability of the bakers payouts
+public enum TzKTBakerServiceType: String, Codable {
+	case tezosOnly = "tezos_only"
+	case multiasset = "multiasset"
+	case exchange = "exchange"
+	case unknown
+}
+
 /// Data representing a baker from TzKT or Baking-Bad
 public struct TzKTBaker: Codable, Hashable {
 	
@@ -48,6 +56,7 @@ public struct TzKTBaker: Codable, Hashable {
 	public let openForDelegation: Bool
 	public let estimatedRoi: Decimal
 	public let serviceHealth: TzKTBakerHealth
+	public let serviceType: TzKTBakerServiceType
 	public let payoutTiming: TzKTBakerTiming
 	public let payoutAccuracy: TzKTBakerAccuracy
 	public let config: TzKTBakerConfig?
@@ -70,12 +79,13 @@ public struct TzKTBaker: Codable, Hashable {
 		self.openForDelegation = false
 		self.estimatedRoi = 0
 		self.serviceHealth = .dead
+		self.serviceType = .unknown
 		self.payoutTiming = .no_data
 		self.payoutAccuracy = .no_data
 		self.config = nil
 	}
 	
-	public init(address: String, name: String?, logo: String?, balance: Decimal, stakingBalance: Decimal, stakingCapacity: Decimal, maxStakingBalance: Decimal, freeSpace: Decimal, fee: Double, minDelegation: Decimal, payoutDelay: Int, payoutPeriod: Int, openForDelegation: Bool, estimatedRoi: Decimal, serviceHealth: TzKTBakerHealth, payoutTiming: TzKTBakerTiming, payoutAccuracy: TzKTBakerAccuracy, config: TzKTBakerConfig?) {
+	public init(address: String, name: String?, logo: String?, balance: Decimal, stakingBalance: Decimal, stakingCapacity: Decimal, maxStakingBalance: Decimal, freeSpace: Decimal, fee: Double, minDelegation: Decimal, payoutDelay: Int, payoutPeriod: Int, openForDelegation: Bool, estimatedRoi: Decimal, serviceHealth: TzKTBakerHealth, serviceType: TzKTBakerServiceType, payoutTiming: TzKTBakerTiming, payoutAccuracy: TzKTBakerAccuracy, config: TzKTBakerConfig?) {
 		
 		self.address = address
 		self.name = name
@@ -92,6 +102,7 @@ public struct TzKTBaker: Codable, Hashable {
 		self.openForDelegation = openForDelegation
 		self.estimatedRoi = estimatedRoi
 		self.serviceHealth = serviceHealth
+		self.serviceType = serviceType
 		self.payoutTiming = payoutTiming
 		self.payoutAccuracy = payoutAccuracy
 		self.config = config
@@ -107,7 +118,7 @@ public struct TzKTBaker: Codable, Hashable {
 		let name = data[1] as? String
 		let normalisedBalance = balance/1000000
 		let normalisedStakingBal = stakingBalance/1000000
-		return TzKTBaker(address: address, name: name, logo: nil, balance: normalisedBalance, stakingBalance: normalisedStakingBal, stakingCapacity: normalisedStakingBal, maxStakingBalance: normalisedStakingBal, freeSpace: normalisedStakingBal, fee: 0.05, minDelegation: 0, payoutDelay: 6, payoutPeriod: 1, openForDelegation: true, estimatedRoi: 0.05, serviceHealth: .active, payoutTiming: .no_data, payoutAccuracy: .no_data, config: nil)
+		return TzKTBaker(address: address, name: name, logo: nil, balance: normalisedBalance, stakingBalance: normalisedStakingBal, stakingCapacity: normalisedStakingBal, maxStakingBalance: normalisedStakingBal, freeSpace: normalisedStakingBal, fee: 0.05, minDelegation: 0, payoutDelay: 6, payoutPeriod: 1, openForDelegation: true, estimatedRoi: 0.05, serviceHealth: .active, serviceType: .tezosOnly, payoutTiming: .no_data, payoutAccuracy: .no_data, config: nil)
 	}
 	
 	/// Convert con-chain data into a meaningful, readable object
