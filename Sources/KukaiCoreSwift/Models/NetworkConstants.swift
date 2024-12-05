13017bb24e105be429d0ffd8e9027a14807b4b82
Simon Mcloughlin <simon.d.mcl@gmail.com>
2024-12-05 11:48:52 +0000

    update constants

--- Sources/KukaiCoreSwift/Models/NetworkConstants.swift
+++ Sources/KukaiCoreSwift/Models/NetworkConstants.swift
@@ -16,9 +16,10 @@ public struct NetworkConstants: Codable {
 	public let origination_size: Int
 	public let cost_per_byte: String
 	public let hard_storage_limit_per_operation: String
+	public let blocks_per_cycle: Int
 	
 	public func secondsBetweenBlocks() -> Int {
-		return Int(minimal_block_delay) ?? 30
+		return Int(minimal_block_delay) ?? 10
 	}
 	
 	public func mutezPerByte() -> Int {
